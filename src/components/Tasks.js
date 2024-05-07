3bcc7cafc1dcb055e8b23b4298af2604c50fde03
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2024-05-07 23:19:37 +0530

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,17 @@
-// use rafce
-
-// import {useState} from 'react'           // useState is a hook <--- read it
 import Task from './Task'
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
     
   return (
     <>
-    {tasks.map((task) => (                  // .map() <--- read it
+    {tasks.map((task) => (                 
         <Task 
         key = {task.id} 
-        task = {task}                      // Note: the first task is the property inside <Task> componenet whereas  
-                                           //       the second one is the parameter passed from tasks.map((task)).
+        task = {task}                     
         onDelete = {onDelete} 
         onToggle = {onToggle}                    
-        />
-            
-            
-        )                                 // 'key' prop <--- as in here map create/iterating though a list so each list should be
-                                          //                 unique that's why key prop is used
-                                          // Note: this 'key' prop is not mandatory but it will show error in console(f12)
+        />          
+        )                                 
         )}
     </>
   )
