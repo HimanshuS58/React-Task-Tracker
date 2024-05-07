c354a6386baab7d8ca99272041cb8c344b600d10
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2024-05-07 23:16:45 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,11 @@
-// use rafce
-
-// this componenet and About.js are used in Routing
-
-import { Link } from 'react-router-dom'         // this {Link} hook is used so that when we click on 'About' link
-                                                // it doesn't refresh/load the page
+import { Link } from 'react-router-dom'         
 
 const Footer = () => {
   return (
     <footer>
         <p>Copyright &copy; 2024 </p>
         <Link to="/about">About</Link>           
-        </footer>                          // instead of <a href = ""></a> we are using <Link to = ""></Link>
+        </footer>                          
   )
 }
 
