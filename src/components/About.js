3c1f03165394291a8cb60cdd0ecc0d5996aff819
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2024-05-07 23:15:43 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,12 @@
-// use rafce
 
-// this componenet and Footer.js are used in Routing
-
-import { Link } from 'react-router-dom'              // this {Link} hook is used so that when we click on 'Go Back' link
-                                                     // it doesn't refresh/load the page
+import { Link } from 'react-router-dom'       
 
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
       <Link to = "/">Go Back</Link>                
-    </div>                                         // Instead of <a href = ""></a> we are using <Link to = ""></Link>
-                                                   // Note: Instead of "/" you can also use "http://localhost:3000"
+    </div>                                        
   )
 }
 
