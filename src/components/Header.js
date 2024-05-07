893d5ccf1a1defbdbf38a4fef3593a52304fd699
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2024-05-07 23:18:48 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,22 @@
-// use rafce
-
-import PropTypes from 'prop-types'      // for practice
+import PropTypes from 'prop-types'      
 import Button from './Button'
 
-import { useLocation } from 'react-router-dom'      // this hook is used so that when you click on 'About' link
-                                                    // the "Add" button didn't show up.
+import { useLocation } from 'react-router-dom'      .
 
 const Header = ({ title, onAdd, showAdd }) => {     
   // const onClick = () => {
-  //   console.log('click')                 // <--- for practice
+  //   console.log('click')                
   // }  
 
    const location = useLocation()  
 
   return (
     <header className = 'header'>
-      {/* <h1 style = {headingStyles}> {title} </h1> */}
-      {/* <h1 style = {{color: 'red', backgroundColor: 'black'}}> {title} </h1> */}
-
-      <h1> {title} </h1>       {/* Note: Here we are not providing any value to title prop. 
-                                         The title here is given using below 'Header.defaultProps' */}
+   
+      <h1> {title} </h1>      
 
                                          
-      {location.pathname === '/' &&                          // "&&" is used in place of ternary (?) when (:) is empty.
+      {location.pathname === '/' &&                          
           <Button 
                  color = {showAdd ? 'red' : 'green'} 
                  text = {showAdd ? 'Close' : 'Add'}
@@ -34,25 +28,15 @@ const Header = ({ title, onAdd, showAdd }) => {
   )
 }
 
-// for practice
+
 Header.defaultProps = {
   title: 'Task Tracker'
 }
 
-// for practice
-Header.propTypes = {                     // for this u have to "import PropTypes from 'prop-types'" <--- given above
 
-  title: PropTypes.string.isRequired     // if we pass anything in title other than a string it will output that result
-                                         // but will show warning/error in components(using f12) stating the mistake.
-                                         // This is helpful in making your code more robust and catch error.
+Header.propTypes = {                    
+  title: PropTypes.string.isRequired    
 }
-
-// CSS in JS <---- for practice
-// const headingStyles = {
-//   color: 'red',
-//   backgroundColor: 'black'
-// }
-
 
 export default Header
 
